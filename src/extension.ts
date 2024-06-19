f88805841da4689210fed758dcdfe70693fa1d5a
yeon <ohy9205@naver.com>
2024-06-19 11:40:58 +0900

    파일 확장자별로 주석마크 구분

--- src/extension.ts
+++ src/extension.ts
@@ -13,16 +13,22 @@ export function activate(context: vscode.ExtensionContext) {
         let documentText = editor.document.getText();
 
         // 프로젝트 기준 파일의 상대경로를 들고옴
-        const filename = vscode.workspace.asRelativePath(
+        const filePath = vscode.workspace.asRelativePath(
           editor.document.uri.fsPath
         );
 
+        // 파일 확장자별로 주석문법을 구분함
+        const mark = getCommentMark(filePath, "{{__filename__}}");
+
         // 파일내용 최상단에 파일경로를 추가함
-        const documentTextWithFilename = `//${filename} \n ${documentText}`;
+        const documentTextWithFilename = `${mark.replace(
+          "{{__filename__}}",
+          filePath // 임시 파일명을 진짜 파일명으로 치환
+        )} \n ${documentText}`;
 
         // 파일명이 포함된 파일내용 복사
         vscode.env.clipboard.writeText(documentTextWithFilename).then(() => {
-          vscode.window.showInformationMessage(`Copied file name: ${filename}`);
+          vscode.window.showInformationMessage(`Copied file name: ${filePath}`);
         });
       } else {
         vscode.window.showInformationMessage("Copy failed");
@@ -33,3 +39,36 @@ export function activate(context: vscode.ExtensionContext) {
   // copyWithFilenameInMenu 구독
   context.subscriptions.push(copyWithFilename);
 }
+
+const getCommentMark = (filePath: string, tempFilename: string) => {
+  const fileType = filePath.split(".").pop();
+  switch (fileType) {
+    case "js":
+    case "ts":
+    case "java":
+    case "jsx":
+    case "tsx":
+    case "c":
+    case "cpp":
+    case "cs":
+    case "php":
+      return `// ${tempFilename}`;
+    case "jsp":
+      return `<%-- ${tempFilename} --%>`;
+    case "html":
+    case "xml":
+    case "md":
+      return `<!-- ${tempFilename} -->`;
+    case "css":
+      return `/* ${tempFilename} */`;
+    case "asp":
+      return `' ${tempFilename}`;
+    case "yml":
+    case "yaml":
+    case "sh":
+    case "py":
+      return `# ${tempFilename}`;
+    default:
+      return `// ${tempFilename}`;
+  }
+};
