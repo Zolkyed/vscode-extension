2e106e0d2cfb47b7e218058bbeedd5c5b4f3c73f
yeon <ohy9205@naver.com>
2024-06-19 10:52:48 +0900

    빈파일도 복사되도록 수정

--- src/extension.ts
+++ src/extension.ts
@@ -12,22 +12,18 @@ export function activate(context: vscode.ExtensionContext) {
         // 파일내용을 가져옴
         let documentText = editor.document.getText();
 
-        if (documentText) {
-          // 프로젝트 기준 파일의 상대경로를 들고옴
-          const filename = vscode.workspace.asRelativePath(
-            editor.document.uri.fsPath
-          );
+        // 프로젝트 기준 파일의 상대경로를 들고옴
+        const filename = vscode.workspace.asRelativePath(
+          editor.document.uri.fsPath
+        );
 
-          // 파일내용 최상단에 파일경로를 추가함
-          const documentTextWithFilename = `//${filename} \n ${documentText}`;
+        // 파일내용 최상단에 파일경로를 추가함
+        const documentTextWithFilename = `//${filename} \n ${documentText}`;
 
-          // 파일명이 포함된 파일내용 복사
-          vscode.env.clipboard.writeText(documentTextWithFilename).then(() => {
-            vscode.window.showInformationMessage(
-              `Copied file name: ${filename}`
-            );
-          });
-        }
+        // 파일명이 포함된 파일내용 복사
+        vscode.env.clipboard.writeText(documentTextWithFilename).then(() => {
+          vscode.window.showInformationMessage(`Copied file name: ${filename}`);
+        });
       } else {
         vscode.window.showInformationMessage("Copy failed");
       }
