371a6ea1e13cfab33e69ff3d991bd08fe85278cf
Zolkyed <zolkyed@gmail.com>
2025-04-23 16:27:57 -0400

    update: README.md

--- src/extension.ts
+++ src/extension.ts
@@ -1,42 +1,30 @@
 import * as vscode from "vscode";
 
 export function activate(context: vscode.ExtensionContext) {
-  // 파일명과 함께 내용복사 실행
   let copyWithFilename = vscode.commands.registerCommand(
     "copy-with-filename.copy-with-filename",
     () => {
-      // 파일의 내용을 들고오기 위해 에디터를 이용함
       const editor = vscode.window.activeTextEditor;
 
       if (editor) {
-        // 파일내용을 가져옴
-        let documentText = editor.document.getText();
+        let documentText = editor.document.getText(editor.selection);
 
-        // 프로젝트 기준 파일의 상대경로를 들고옴
         const filePath = vscode.workspace.asRelativePath(
           editor.document.uri.fsPath
         );
 
-        // 파일 확장자별로 주석문법을 구분함
         const mark = getCommentMark(filePath, "{{__filename__}}");
 
-        // 파일내용 최상단에 파일경로를 추가함
         const documentTextWithFilename = `${mark.replace(
           "{{__filename__}}",
-          filePath // 임시 파일명을 진짜 파일명으로 치환
-        )} \n ${documentText}`;
+          filePath 
+        )} \n${documentText}`;
 
-        // 파일명이 포함된 파일내용 복사
-        vscode.env.clipboard.writeText(documentTextWithFilename).then(() => {
-          vscode.window.showInformationMessage(`Copied file name: ${filePath}`);
-        });
-      } else {
-        vscode.window.showInformationMessage("Copy failed");
+        vscode.env.clipboard.writeText(documentTextWithFilename);
       }
     }
   );
 
-  // copyWithFilenameInMenu 구독
   context.subscriptions.push(copyWithFilename);
 }
 
